69cbbdbc7cb429b3904b5ccbaed23f7af084c9f6
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2016-11-13 18:30:06 +0100

    fix(extend): The extend no longer disturb the power wheel control

---
+++ src/basic_robot_pkg/script/retract.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rosh
+import time
+import rospy
+
+rosh.rosh_init()
+
+power = 0
+steer = 0.0
+
+
+power_cmd = rosh.msg.std_msgs.Float64()
+steer_cmd = rosh.msg.std_msgs.Float64()
+
+power_cmd.data = power
+steer_cmd.data = steer
+
+left_steer = getattr(rosh.topics, '/robot_gazebo/left_wheel_steering_controller/command')
+left_power = getattr(rosh.topics, '/robot_gazebo/left_wheel_power_controller/command')
+right_steer = getattr(rosh.topics, '/robot_gazebo/right_wheel_steering_controller/command')
+right_power = getattr(rosh.topics, '/robot_gazebo/right_wheel_power_controller/command')
+
+extend_cmd = getattr(rosh.topics, '/robot_gazebo/central_cylinder_controller/command')
+
+
+while True:
+
+    left_steer(steer_cmd)
+    right_steer(steer_cmd)
+    left_power(power_cmd)
+    right_power(power_cmd)
+    extend_cmd(0.0)
+    time.sleep(0.01)
